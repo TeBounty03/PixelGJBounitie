1b806c28788ef2626b9a90668ca10494397e8696
Yunitie <lou.morin.perso@gmail.com>
2024-05-12 18:43:56 +0200

    [FIX] Player link Text Health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
@@ -11,6 +10,39 @@ public class PlayerHealth : MonoBehaviour
 
     void Start()
     {
+        //Cette partie sert à trouver le texte des points de vie et à le lier à "healthText"
+        Canvas canvas = FindObjectOfType<Canvas>();
+        if (canvas != null)
+        {
+            // Recherchez l'objet Image dans le Canvas
+            Image image = canvas.GetComponentInChildren<Image>();
+
+            if (image != null && image.name == "Heart_1")
+            {
+                // Recherchez le composant TextMeshProUGUI dans l'objet Image
+                healthText = image.GetComponentInChildren<TextMeshProUGUI>();
+
+                if (healthText != null)
+                {
+                    // Faites quelque chose avec le TextMeshProUGUI trouvé
+                    Debug.Log("TextMeshProUGUI trouvé !");
+                }
+                else
+                {
+                    Debug.LogError("TextMeshProUGUI non trouvé dans l'objet Image.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Image non trouvée dans le Canvas.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Canvas non trouvé dans la scène.");
+        }
+
+
         currentHealth = maxHealth;
         healthText.text = currentHealth.ToString();
     }
