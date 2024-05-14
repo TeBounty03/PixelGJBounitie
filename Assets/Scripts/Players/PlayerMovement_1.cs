8585ea74764bc95648d1908511e333d82b8012da
Yunitie <lou.morin.perso@gmail.com>
2024-05-14 20:48:07 +0200

    [IMP] Better jump P1 & P2

--- Assets/Scripts/Players/PlayerMovement_1.cs
+++ Assets/Scripts/Players/PlayerMovement_1.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement_1 : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
+    public float moveSpeed = 350f;
+    public float jumpForce = 7f;
 
     private bool isJumping;
     public bool isGrounded;
@@ -22,7 +22,7 @@ public class PlayerMovement_1 : MonoBehaviour
 
     void Update()
     {
-
+        //No double jump
         if (Input.GetButtonDown("Jump_1") && isGrounded)
         {
             isJumping = true;
@@ -35,23 +35,19 @@ public class PlayerMovement_1 : MonoBehaviour
         isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
 
         float horizontalMovement = Input.GetAxis("Horizontal_1") * moveSpeed * Time.deltaTime;
-
         MovePlayer(horizontalMovement);
 
+        //Change gravity when falling
+        FallMultiplier();
+
+        //Animation
         float characterVelocityX = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("SpeedX", characterVelocityX);
-
-        //Animations de saut
-        // animator.SetFloat("SpeedY", rb.velocity.y);
-        // animator.SetBool("isGrounded", isGrounded);
-
         Flip(rb.velocity.x);
-        FallMultiplier();
     }
 
     private void FallMultiplier()
     {
-
         if (rb.velocity.y < 0.1f)
         {
             rb.gravityScale = _fallMultiplier;
@@ -68,13 +64,13 @@ public class PlayerMovement_1 : MonoBehaviour
 
     void MovePlayer(float _horizontalMovement)
     {
+        //Horizontal Movement
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
         //Jump
         if (isJumping == true)
         {
-            // rb.AddForce(new Vector2(0f, jumpForce));
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isJumping = false;
         }
--- Assets/Scripts/Players/PlayerMovement_2.cs
+++ Assets/Scripts/Players/PlayerMovement_2.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement_2 : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
+    public float moveSpeed = 350f;
+    public float jumpForce = 7f;
 
     private bool isJumping;
     public bool isGrounded;
@@ -16,10 +16,13 @@ public class PlayerMovement_2 : MonoBehaviour
 
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    [SerializeField] private float _fallMultiplier = 2f;
+    [SerializeField] private float _lowJumpFallMultiplier = 2f;
+
 
     void Update()
     {
-
+        //No double jump
         if (Input.GetButtonDown("Jump_2") && isGrounded)
         {
             isJumping = true;
@@ -32,27 +35,43 @@ public class PlayerMovement_2 : MonoBehaviour
         isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
 
         float horizontalMovement = Input.GetAxis("Horizontal_2") * moveSpeed * Time.deltaTime;
-
         MovePlayer(horizontalMovement);
 
+        //Change gravity when falling
+        FallMultiplier();
+
+        //Animation
         float characterVelocityX = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("SpeedX", characterVelocityX);
-
-        //Animations de saut
-        // animator.SetFloat("SpeedY", rb.velocity.y);
-        // animator.SetBool("isGrounded", isGrounded);
-
         Flip(rb.velocity.x);
+    }
+
+    private void FallMultiplier()
+    {
+        if (rb.velocity.y < 0.1f)
+        {
+            rb.gravityScale = _fallMultiplier;
+        }
+        else if (rb.velocity.y > 0.1f && !Input.GetButton("Jump_2"))
+        {
+            rb.gravityScale = _lowJumpFallMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
     }
 
     void MovePlayer(float _horizontalMovement)
     {
+        //Horizontal Movement
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
+        //Jump
         if (isJumping == true)
         {
-            rb.AddForce(new Vector2(0f, jumpForce));
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isJumping = false;
         }
     }
